24b3e5317f051f99d22931526b0612d8e2df002e
wwatson <wdfwatson@gmail.com>
2023-10-31 00:28:47 -0300

    Basic Height Based Ambient Occlusion

--- ShellTexture.gd
+++ ShellTexture.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @export var color: Color
 @export var numLayers: int = 32;
+@export var strands: int = 100
 @export var height: float = 0.1
 @export var shellTextureShader: Shader
 @export var model: Mesh
@@ -24,7 +25,7 @@ func _ready():
 		print_debug(mesh.get_surface_override_material(0))
 		material.shader = shellTextureShader;
 		material.set_shader_parameter("threshold",threshold)
-		material.set_shader_parameter("strands",100)
+		material.set_shader_parameter("strands",float(strands))
 		material.set_shader_parameter("color",color)
 		add_child(mesh)
 	
